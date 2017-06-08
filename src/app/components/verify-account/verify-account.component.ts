bdeb74865a69eed6c21443834cb9cb7bb248baf9
Alonso Moreno <ramp_alonso@outlook.com>
2017-06-08 10:44:54 -0500

    Interfaz confirmar cuenta terminado

---
+++ src/app/components/verify-account/verify-account.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-account',
+  templateUrl: './verify-account.component.html'
+})
+export class VerifyAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
