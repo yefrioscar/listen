beca45bfab3c72e6f0fb535a48d7eb5c080d9ee7
Alonso Moreno <ramp_alonso@outlook.com>
2017-06-18 18:18:25 -0500

    Validation error yet to up

--- src/app/components/authentication/signup/signup.component.ts
+++ src/app/components/authentication/signup/signup.component.ts
@@ -29,26 +29,20 @@ export class SignupComponent {
 
   }
   
-  signUp(){
-    this.service.signUp(this.dni,this.name,this.lastname,this.email,this.password,this.fecNac).subscribe(
+  signUp(userForm: FormGroup){
+    if(userForm.dirty && userForm.valid){
+      this.service.signUp(this.dni,this.name,this.lastname,this.email,this.password,this.fecNac).subscribe(
       response => {
         console.log(response)
         this.router.navigate(['/home'])
       },
       error => console.log(error)
     )
-  }
-
-  clicked(userForm: FormGroup){
-    if(userForm.dirty && userForm.valid){
-      console.log('Entro y mando, validado los datos y se registro');
+    console.log("Entro y mando, valido y se registro");
     } else{
-      console.log('Error 4head');
-      //Aqui haria la accion de mandar el error exacto
-      //Pero como es validacion del template tendria que obtener los valores con formControl
-      //Pero no se puede usar eso cuando usas el [(ngModel)]... aunque todavia no lo pruebo, acabo de encontrar eso en internet
-      //Si fuera reactiveform, esto sería mas fácil pero no se puede usar directivas con [[ngModel]] :vvvv
+      alert('Completa bien los datos');
     }
+  
   }
   
 
