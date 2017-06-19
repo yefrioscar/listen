b732db38dd9fa368fd1bc5e21f68713d1658d798
Alonso Moreno <ramp_alonso@outlook.com>
2017-06-18 23:51:30 -0500

    Validation signup done. Some issues to fix yet

--- src/app/components/authentication/signup/signup.component.ts
+++ src/app/components/authentication/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, Directive } from '@angular/core';
+import { Component, Directive, OnInit } from '@angular/core';
 import { AuthoService } from '../../../services/autho.service';
 import { User } from '../../../models/user.model';
 import { Router, NavigationExtras } from '@angular/router';
@@ -9,41 +9,92 @@ import { AbstractControl, FormArray, FormControl, FormBuilder, FormGroup, Valida
   selector: 'signup',
   templateUrl: './signup.component.html'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
-    userForm: FormGroup;
-    dni: string;
-    terms:  boolean;
-    email: string;
-    password: string;
-    lastname: string;
-    name: string;
-    fecNac: string;
+  registerForm:FormGroup;
+  dni: string;
 
-  
-  
   constructor(
     private service: AuthoService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ){
 
   }
-  
-  signUp(userForm: FormGroup){
-    if(userForm.dirty && userForm.valid){
-      this.service.signUp(this.dni,this.name,this.lastname,this.email,this.password,this.fecNac).subscribe(
-      response => {
-        console.log(response)
-        this.router.navigate(['/home'])
-      },
-      error => console.log(error)
-    )
-    console.log("Entro y mando, valido y se registro");
-    } else{
-      alert('Completa bien los datos');
-    }
-  
+
+  ngOnInit(){
+  this.registerForm = this.fb.group({
+    name:['',[Validators.required, Validators.minLength(2),Validators.maxLength(10)]],
+    lastname:['',[Validators.required, Validators.minLength(2),Validators.maxLength(10)]],
+    email:['',[Validators.required,]],
+    password:['',[Validators.required, Validators.minLength(8)]],
+    fecNac:['',[Validators.required]],
+    terms:['',[Validators.required]],
+  });
+ }
+
+
+/* -------- ESTA SERIA LA FORMA 1 -------------
+
+ signUp(registerForm: FormGroup){
+   let name = registerForm.controls.name.value;
+   let lastname = registerForm.controls.lastname.value;
+   let email = registerForm.controls.email.value;
+   let password = registerForm.controls.password.value;
+   let fecNac = registerForm.controls.fecNac.value;
+
+  if(registerForm.dirty && registerForm.valid){
+    this.service.signUp(this.dni,name,lastname,email,password,fecNac).subscribe(
+       response => {
+         console.log(response)
+         this.router.navigate(['/home'])
+       },
+       error => console.log(error)
+     )
+     console.log("Entro y mando, valido y se registro");
   }
-  
+  else{
+    console.log("Error");
+    registerForm.controls.name.markAsTouched();
+    registerForm.controls.lastname.markAsTouched();
+    registerForm.controls.email.markAsTouched();
+    registerForm.controls.password.markAsTouched();
+    registerForm.controls.fecNac.markAsTouched();
+  }
+ }
+ 
+ */
+
+    // dni: string; ESTA SERIA LA FORMA 2
+    name: string;
+    lastname: string;
+    email: string;
+    password: string;
+    fecNac: string;
+    terms: boolean;
+
+  signUp(registerForm: FormGroup){
+
+  if(registerForm.dirty && registerForm.valid){
+    this.service.signUp(this.dni,this.name,this.lastname,this.email,this.password,this.fecNac).subscribe(
+       response => {
+         console.log(response)
+         this.router.navigate(['/home'])
+       },
+       error => console.log(error)
+     )
+     console.log("Entro y mando, valido y se registro");
+  }
+  else{
+    console.log("Error");
+    registerForm.controls.name.markAsTouched();
+    registerForm.controls.lastname.markAsTouched();
+    registerForm.controls.email.markAsTouched();
+    registerForm.controls.password.markAsTouched();
+    registerForm.controls.fecNac.markAsTouched();
+  }
+ }
+
+ 
 
 }
