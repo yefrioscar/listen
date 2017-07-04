2fed3a106d2a92ddf30f22e3ccd9d4908ae9a4dd
Yefri Laura <yefrioscar9814@gmail.com>
2017-07-04 12:36:40 -0500

    First socket

--- src/app/components/app/home/home.component.ts
+++ src/app/components/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthoService } from '../../../services/autho.service';
 import { IdeasService } from '../../../services/ideas.service';
+import { SocketService } from '../../../services/socket.service';
+
 import { Router, NavigationExtras } from '@angular/router';
 import { Idea } from '../../../models/idea.model';
 
@@ -13,25 +15,39 @@ export class HomeComponent implements OnInit {
   ideas: Idea[] = [];
   constructor(private serviceAutho: AuthoService,
               private serviceIdeas: IdeasService,
-              private router: Router ) { }
+              private router: Router,
+              private socket: SocketService ) { }
 
   ngOnInit() {
     this.serviceAutho.getCurrentUser();
     this.getIdeas();
+
+
+    this.socket.connectSocket();
+    
+
+    this.socket.receiveMessages().subscribe(response => {
+      console.log(response);
+		});
   }
 
+  
 
   getIdeas() {
+    this.socket.receiveMessages();
     this.serviceIdeas.getIdeas().subscribe(
         response => {
           console.log(response);
           this.ideas = response;
+          this.socket.sendMessage(this.ideas[0]);
         },
         error => {
           console.log(error);
         }
       );
   }
+
+
 
   ObjectToArray(array: Idea[], object: Idea) {
     for (const key in object) {
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -2,12 +2,38 @@ import { Injectable } from '@angular/core';
 
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
- 
+import { Idea } from '../models/idea.model';
 import * as io from 'socket.io-client';
 
 @Injectable()
 export class SocketService {
 
+  private BASE_URL = 'http://localhost:8080';
+  private socket;
+
   constructor() { }
+
+  connectSocket(){
+  		this.socket = io(this.BASE_URL, { 'forceNew':true });
+  	}
+
+    sendMessage(idea:Idea):void{
+		  this.socket.emit('add-message', idea);
+	  }
+
+
+  receiveMessages():any{ 
+		let observable = new Observable(observer => {
+			this.socket.on('add-message-response', (data) => {
+				observer.next(data); 
+        console.log(data);
+			});
+ 
+			return () => {
+				this.socket.disconnect();
+			}; 
+		}); 
+		return observable;
+	}
 
 }
